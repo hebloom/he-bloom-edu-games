295ac7e146389e2784de933c8641b8442c296357
hebloom <rini.ening@yahoo.com>
2026-09-18 21:19:05 +0800

    update

--- script.js
+++ script.js
@@ -343,3 +343,24 @@ backToLogin.addEventListener("click", () => {
   message.textContent = "";
   
 });
+
+
+
+const togglePassword = document.getElementById("togglePassword");
+const passwordInput = document.getElementById("password");
+
+togglePassword.addEventListener("click", function () {
+
+    if (passwordInput.type === "password") {
+
+        passwordInput.type = "text";
+        togglePassword.textContent = "🔓";
+
+    } else {
+
+        passwordInput.type = "password";
+        togglePassword.textContent = "🔒";
+
+    }
+
+});
